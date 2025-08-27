90595472932ea64af6058eafdd3715736b6b455b
Udara Jayawardena <udarajayawardena5@gmail.com>
2025-08-27 14:55:39 +0530

    feat: tweet-controller-service-added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TweetController } from './tweet/tweet.controller';
+import { TweetService } from './tweet/tweet.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TweetController],
+  providers: [AppService, TweetService],
 })
 export class AppModule {}
